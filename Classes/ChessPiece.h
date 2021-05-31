1eb7794f5dffe995e0f64964222df3477e7c52dc
sayroll <83714631+sayroll@users.noreply.github.com>
2021-06-01 00:25:16 +0800

    Update ChessPiece.h

--- Classes/ChessPiece.h
+++ Classes/ChessPiece.h
@@ -1,98 +1,163 @@
 /********************************************
  * 功能：棋子模型
- * 作者：hjh
- * 版本：1.0.0
+ * 作者：刘兴源
+ * 版本：1.2.0
  * 位置：Classes/model(筛选器)
  * 游戏引擎：Cocos2d-x 4.0
  * ******************************************
- * 说明：目前尚处于设计阶段，尽量多设数据成员
- * 及接口。
+ * 更新内容：合并了现有的两份棋子类内容，并在
+ * 必要处加以修改，仍有函数的具体实现没有写完，
+ * 细化将在此后几天完成。
  ********************************************/
 #pragma once
 #ifndef _CHESSPIECE_H_
 #define _CHESSPIECE_H_
 
-#include <cocos2d.h>
+#include "cocos2d.h"
+#include "cocos/2d/CCAction.h"
+#include "cocos/2d/CCActionInterval.h"
 USING_NS_CC;
+
+#include "ConfigController.h"
+#include "ChessBoard.h"
+#include "Equipment.h"
+#include "Condition.h"
 
 #include <string>
 using std::string;
 
-// 由于只有三个星级，采用枚举控制
-enum level { Level1, Level2, Level3 };
-
-struct PieceInfo // 当前棋子数值结构体
+struct PieceInfo // 棋子数据类，这里存放的是会随战斗进行而改变的数据
 {
-	// 当前血量
-	double healthPoint;
+	double healthPoint; // 生命
+	double healthPointM;//最大生命值
+	
+	double magicPoint; // 法力
+	double magicPoint; // 最大法力
 
-	// 当前能量
-	double magicPoint;
+	double attack; // 攻击力
+	double equipAttack;//装备+初始
+	
+	double defence; // 防御力
+	double equpiDefence;//装备+初始
 
-	// 当前攻击力
-	double attack;
+	double attackSpeed; // 攻击速度
+	double equipAttackSpeed; //装备 + 初始
 
-	// 当前防御力
-	double defence;
+	double attackScope; // 攻击距离
 
-	// 当前攻击速度
-	double attackSpeed;
+	double criticalChance; // 暴击几率
+
+	double criticalDamage; // 暴击伤害
 };
+
+enum class CoordinateType{ real, logical }; // 坐标类型
+
+class PieceCoordinate // 棋子坐标
+{
+public:
+	// 获取棋子横坐标
+	int getX() const { return _x; }
+
+	// 获取棋子纵坐标
+	int getY() const { return _y; }
+
+	// 设置棋子横坐标
+	void setX(const int x) { _x = x; }
+
+	// 设置棋子纵坐标
+	void setY(const int y) { _y = y; }
+
+private:
+	int _x; // 横坐标
+
+	int _y; // 纵坐标
+
+	CoordinateType _type; // 坐标类型
+};
+
+// 由于只有三个星级，采用枚举控制
+enum class Level { level1, level2, level3 };
 
 class ChessPiece : public Ref
 {
 public:
+	// 初始化棋子状态
+	virtual bool init(int id);
 	virtual bool init();
+
+	// 初始化棋子状态
+	void initPieceIfo(int id);
+
+	// 更新棋子状态
+	bool updatePieceInfo(const double damage, PieceCoordinate* newRealCoordinate);
+
+	// 获取棋子名称
+	const string getPieceName() { return _pieceName; }
+
+	// 获取当前棋子数值
+	const PieceInfo* getCrtPieceCondition() { return &_pieceCrtCondition; }
+
+	// 获取当前棋子星级
+	const Level getPieceLevel() { return _pieceLevel; }
+
+	// 获取当前棋子位置
+	const PieceCoordinate* getPrtCoordinateByType(CoordinateType type);
+
+	// 设置当前棋子星级
+	void setPieceLevel(const Level newLevel) { _pieceLevel = newLevel; }
 
 	CREATE_FUNC(ChessPiece);
 
-	// 初始化棋子状态
-	bool initialPieceInfo();
+	//技能函数，继承
+	virtual void skill() = 0;
+	
+	//羁绊 继承
+	virtual void familyBuff() = 0;
 
-	// 更新棋子状态
-	bool updatePieceInfo(const double damage);
+	//提升星级
+	virtual void promoteRank() = 0;
 
-	// 获取棋子名称
-	const string getPieceName() { return pieceName; }
+	//提供装备
+	virtual void giveEquip(int equipNum, int equipType);
 
-	// 获取当前棋子状态结构体
-	const PieceInfo* getCrtPieceCondition() { return &pieceCrtCondition; }
+	//计算buff并修改自身属性
+	virtual void readCondition();
 
-	// 获取当前棋子星级
-	const int getPieceLevel() { return pieceLevel; }
+	virtual void setCondition(double s,int Condition);
 
-	// 设置当前棋子星级
-	void setPieceLevel(const int newLevel) { pieceLevel = newLevel; }
+	//返回攻击属性
+	int myAttack();
+
+	//攻击：吸血函数
+	int attackBack(int blood);
+
+	//攻击：被攻击函数,根据对方hp和血量扣除自身的hp并且返回伤害
+	int beenAttack(int attack);
+
+	//上面攻击函数的package，攻击在这里调用，参数A即为被攻击的对象
+	void attackOne(ChessPiece A);
+
+	//判断棋子是否死了
+	bool ifDead();
+
 private:
-	// 名称
-	string pieceName;
+	string _pieceName; // 名称
+	
+	string _piecePicPath; // 模型图片相对路径
+	
+	string _pieceDataPath; // 基础数据在库中的key
+	
+	Level _pieceLevel; // 星级
 
-	// 星级
-	int pieceLevel;
+	int _piecePerCost; // 一只一星棋子所需金币
 
-	// 当前棋子数值
-	PieceInfo pieceCrtCondition;
+	PieceInfo _pieceCrtCondition; // 当前棋子数值
+	
+	PieceInfo _buffEffect; // 当前装备(及羁绊？)效果加成
 
-	// 当前装备效果
-	PieceInfo equipEffect;
+	PieceCoordinate _logCoordinate; // 棋子的逻辑位置
 
-	// 一只一星棋子所需金币
-	int piecePerCost;
-
-	// 各等级基础血量
-	double pieceBaseHealthPoint[3];
-
-	// 各等级基础能量
-	double pieceBaseMagicPoint[3];
-
-	// 各等级基础攻击力
-	double pieceBaseAttack[3];
-
-	// 各等级基础防御力
-	double pieceBaseDefence[3];
-
-	// 各等级基础攻击速度
-	double pieceBaseAttackSpeed[3];
+	PieceCoordinate _realCoordinate; // 棋子的实际位置
 };
 
 #endif

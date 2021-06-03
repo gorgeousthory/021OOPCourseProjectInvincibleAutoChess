e49e6bc6b91470028f27f04fff71ec94d85d7998
sayroll <83714631+sayroll@users.noreply.github.com>
2021-06-03 21:19:25 +0800

    Update ChessPiece.cpp

--- Classes/ChessPiece.cpp
+++ Classes/ChessPiece.cpp
@@ -77,3 +77,92 @@ bool ChessPiece::updatePieceInfo(const double damage, PieceCoordinate* newRealCo
 	// 动画应该在PlayScene中创建，这里先放在这里（版本1.1.1）
 	auto updatePiecePos = MoveTo::create(time, Vec2(newRealCoordinate->getX(), newRealCoordinate->getY()));
 }
+
+bool ChessPiece::ifDead()
+{
+	return _pieceCrtCondition.healthPoint > 0 ? true : false;
+}
+
+
+void ChessPiece::readCondition()
+{
+	//实际战斗中，一个棋子的真实属性均在equipXX中储存，Attack/defence这些数据均是一个基础数据，基本不调用
+	//有攻击加成buff的攻击力
+	_pieceCrtCondition.equipAttack = _pieceCrtCondition.equipAttack * (1 + _pieceCrtCondition.myCondition.getInspireAttack());
+
+	//有防御加成
+	_pieceCrtCondition.equpiDefence = _pieceCrtCondition.equpiDefence * (1 + _pieceCrtCondition.myCondition.getInspireDefence());
+
+	//攻速加成
+	_pieceCrtCondition.equipAttackSpeed = _pieceCrtCondition.equipAttackSpeed * (1 + _pieceCrtCondition.myCondition.getInspireSpeed());
+}
+
+void ChessPiece::setCondition(double s, int Condition)
+{
+	switch (Condition)
+	{
+	case 1:
+		_pieceCrtCondition.myCondition.controlSleep(s);
+		break;
+	case 2:
+		_pieceCrtCondition.myCondition.controlFragile(s);
+		break;
+	case 3:
+		_pieceCrtCondition.myCondition.controlInspireAttack(s);
+		break;
+	case 4:
+		_pieceCrtCondition.myCondition.controlInspireSpeed(s);
+		break;
+	case 5:
+		_pieceCrtCondition.myCondition.controlInspireDefence(s);
+		break;
+	case 6:
+		_pieceCrtCondition.myCondition.controlSuck(s);
+		break;
+	case 7:
+		_pieceCrtCondition.myCondition.controlGetHp(s);
+		break;
+	}
+}
+
+int ChessPiece::myAttack()
+{
+	srand(time(NULL));
+	int rate = rand() % 100 + 1;
+	if (rate >= _pieceCrtCondition.criticalChance) {//暴击了
+		return 2 * _pieceCrtCondition.equipAttack;
+	}
+	else {//没暴击
+		return _pieceCrtCondition.equipAttack;
+	}
+}
+
+//blood指一次攻击产生的伤害，hp是攻击以后的血量
+int ChessPiece::attackBack(int blood)
+{
+	int hp = 0;//指攻击回复的hp
+	hp = _pieceCrtCondition.healthPoint + blood * _pieceCrtCondition.myCondition.getSuck();
+	if (hp >= _pieceCrtCondition.healthPointM) {
+		_pieceCrtCondition.healthPoint = _pieceCrtCondition.healthPointM;
+		return  _pieceCrtCondition.healthPointM;
+	}
+	else {
+		_pieceCrtCondition.healthPoint = hp;
+		return hp;
+	}
+}
+
+//返回受到的伤害
+int ChessPiece::beenAttack(int attack)
+{
+	double defenceRate = _pieceCrtCondition.defence / (1 + _pieceCrtCondition.defence);
+	int damage = attack * defenceRate * _pieceCrtCondition.myCondition.getFragile();//总共伤害
+	_pieceCrtCondition.healthPoint -= damage;
+	return damage;
+}
+
+void ChessPiece::attackOne(ChessPiece& A)
+{
+	//回血&&给对象A打伤害
+	attackBack(A.beenAttack(myAttack()));
+}
